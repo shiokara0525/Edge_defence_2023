33f6c52d54bb54c6e1738a07f3581ede260b2bcb
yuta <yuta0525ny@gmail.com>
2024-03-10 12:02:17 +0900

    add ダイセンモーター仕様　マクソンにするときはぜったい直して

--- lib/motor_a/motor_a.cpp
+++ lib/motor_a/motor_a.cpp
@@ -10,7 +10,7 @@ motor_attack::motor_attack(){
   for(int i = 0; i < 4; i++){
     Motor[i].setLenth(motor_max);
   }
-  analogWriteFrequency(3,90000);
+  // analogWriteFrequency(3,90000);
     //モーターのピンと行列式に使う定数の設定
 }
 
